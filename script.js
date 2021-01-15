438cce5750f7975f647597bf5cd8a21b9da61c08
Dana Corona <dana.d.corona@gmail.com>
2021-01-15 15:29:32 -0600

    Found an error that was messing with my code

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 $(document).ready(function() {
-// this is my api key
-var apiKey="3022b7a270f3e92e920047814c11ced1";
+    // this is my api key
+    var apiKey="3022b7a270f3e92e920047814c11ced1";
 
 
 
-$("button").on("click", function(event){
-    event.preventDefault();
-    console.log("working");
-    getWeather($("#searchArea").val());
-});
+    $("button").on("click", function(event){
+        event.preventDefault();
+        getWeather($("#searchArea").val());
+        // getForecast($("#searchArea").val());
+    
+    });
 
-
-// the ajax method to load content
-function getWeather(value) {
-    $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather?q="+ value +"&appid=" + apiKey + "&units=imperial",
-        method: "GET"
-    }).then(function(response) {
-        // Update these with classes needed
-        $(".city").text(response.name);
-        $("#weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon +".png");
-        $(".temp").text("Temperature: " + response.main.temp + " F");
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        $(".windspeed").text("Wind Speed: " + response.wind.speed + "mph");
-        // $(".uvindex").text(response.name);
+    // let uvi = "";
         
-        console.log(response.weather[0].icon);
-        console.log(current.uvi);
 
-    })
-}
+    // the ajax method to load content
+    function getWeather(val) {
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q="+ val +"&appid=" + apiKey + "&units=imperial",
+            method: "GET"
+        }).then(function(response) {
+            $(".city").text(response.name);
+            $("#weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon +".png");
+            $(".temp").text("Temperature: " + response.main.temp + " F");
+            $(".humidity").text("Humidity: " + response.main.humidity);
+            $(".windspeed").text("Wind Speed: " + response.wind.speed + " mph");
 
+            $("div #cityStorage").append(`<li> ${val} </li>`);
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            getUVI(lat, lon);
 
+        });
+    };
+
+    function getUVI (lat, lon) {
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi?lat="+ lat + "&lon=" + lon + "&appid=" + apiKey,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            console.log(response.value);
+            $("#uvi").text("UVI: " + response.value);
+        });
+
+    }
 });
-
-
-
