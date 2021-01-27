b458095dc124deade6cd07c1fcc720943b018941
Dana Corona <dana.d.corona@gmail.com>
2021-01-27 00:36:13 -0600

    Completed the code with the 5 day Forecast and updated the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-Dashboard
+
+This weather dashboard was created for the user to be able to put in a city into the search area and find out the current weather for said city. It will give the user the temperature, humidity, windspeed, and will let the user know the UVI conditions via color coding. The user will also see the forecast for the next 5 days as well. 
+
+[Check out the Weather Dashboard here](https://danacorona.github.io/Weather-Dashboard/)
+
+# Screenshots
+![An Image of the Weather Dashboard](./images/Weather-Dashboard.png)
--- script.js
+++ script.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     
     });
 
-    // let uvi = "";
         
 
     // the ajax method to load content
@@ -29,6 +28,7 @@ $(document).ready(function() {
             $("div #cityStorage").append(`<li> ${val} </li>`);
             var lat = response.coord.lat;
             var lon = response.coord.lon;
+            console.log(response);
             getUVI(lat, lon);
 
         });
@@ -40,9 +40,57 @@ $(document).ready(function() {
             method: "GET"
         }).then(function(response) {
             console.log(response);
-            console.log(response.value);
-            $("#uvi").text("UVI: " + response.value);
+            var uvi = response.value;
+            $("#uvi").text("UVI: " + uvi);
+            $(".date").text("Date: " + response.date_iso);
+
+            // add the conditions for the uvi styling
+            if (uvi < 3) {
+                $("#uvi").addClass("low");
+
+            } else if (uvi > 3 && uvi < 5) {
+                $("#uvi").addClass("moderate");
+            } else {
+                $("#uvi").addClass("severe");
+            }
+
+            getFuture(lat, lon);
         });
 
     }
+    function getFuture(lat, lon) {
+        $.ajax ({
+            url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial",
+            method: "GET"
+        }).then(function(response){
+            // Day 1
+            $("#day1date").text("Date: " + response.list[2].dt_txt);
+            $("#weathericon1").attr("src", "http://openweathermap.org/img/wn/" + response.list[2].weather[0].icon +".png");
+            $("#day1temp").text("Temp: " + response.list[2].main.temp);
+            $("#day1humidity").text("Humidity: " + response.list[2].main.humidity);
+
+            //Day 2
+            $("#day2date").text("Date: " + response.list[10].dt_txt);
+            $("#weathericon2").attr("src", "http://openweathermap.org/img/wn/" + response.list[10].weather[0].icon +".png");
+            $("#day2temp").text("Temp: " + response.list[10].main.temp);
+            $("#day2humidity").text("Humidity: " + response.list[10].main.humidity);
+            //Day 3
+            $("#day3date").text("Date: " + response.list[18].dt_txt);
+            $("#weathericon3").attr("src", "http://openweathermap.org/img/wn/" + response.list[18].weather[0].icon +".png");
+            $("#day3temp").text("Temp: " + response.list[18].main.temp);
+            $("#day3humidity").text("Humidity: " + response.list[18].main.humidity);
+            //Day 4
+            $("#day4date").text("Date: " + response.list[26].dt_txt);
+            $("#weathericon4").attr("src", "http://openweathermap.org/img/wn/" + response.list[26].weather[0].icon +".png");
+            $("#day4temp").text("Temp: " + response.list[26].main.temp);
+            $("#day4humidity").text("Humidity: " + response.list[26].main.humidity);
+            //Day 5
+            $("#day5date").text("Date: " + response.list[34].dt_txt);
+            $("#weathericon5").attr("src", "http://openweathermap.org/img/wn/" + response.list[34].weather[0].icon +".png");
+            $("#day5temp").text("Temp: " + response.list[34].main.temp);
+            $("#day5humidity").text("Humidity: " + response.list[34].main.humidity);
+        })
+    };
+
+
 });
